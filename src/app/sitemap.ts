65d857d3c16359af82ba6fd0d5673b3be83d7c06
missionevangeliquesalem10-dev <tadigitalsolution10@gmail.com>
2026-01-23 17:58:21 +0000

    SEO: ajout fichier verification google

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,11 +2,41 @@ import { MetadataRoute } from 'next'
  
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
-    { url: 'https://www.ivsc-iverservices.com', lastModified: new Date() },
-    { url: 'https://www.ivsc-iverservices.com/automobile', lastModified: new Date() },
-    { url: 'https://www.ivsc-iverservices.com/assurances', lastModified: new Date() },
-    { url: 'https://www.ivsc-iverservices.com/banque', lastModified: new Date() },
-    { url: 'https://www.ivsc-iverservices.com/finance', lastModified: new Date() },
-    { url: 'https://www.ivsc-iverservices.com/contact', lastModified: new Date() },
+    {
+      url: 'https://www.ivsc-iverservices.com',
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 1,
+    },
+    {
+      url: 'https://www.ivsc-iverservices.com/automobile',
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 0.8,
+    },
+    {
+      url: 'https://www.ivsc-iverservices.com/assurances',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://www.ivsc-iverservices.com/banque',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://www.ivsc-iverservices.com/finance',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://www.ivsc-iverservices.com/contact',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.5,
+    },
   ]
 }
